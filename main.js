56151515b018256fcc8cb65908c10a63027121f6
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-01-10 13:57:21 +0200

    Added Main Project

---
+++ main.js
@@ -0,0 +1,128 @@
+listData();
+let pageNum = 1;
+let imageNum = 001;
+
+document.querySelector(".next").onclick = () => {
+  pageNum += 1;
+  if (pageNum > 1) {
+    document.querySelector(".back").classList.add("active");
+  }
+
+  if (pageNum < 10) {
+    imageNum = "00" + pageNum;
+  } else if (pageNum < 100) {
+    imageNum = `0${pageNum}`;
+  } else {
+    imageNum = pageNum;
+  }
+
+  let image = document.querySelector(".img img");
+
+  image.setAttribute(
+    "src",
+    `https://www.searchtruth.org/quran/images1/${imageNum}.jpg`
+  );
+};
+
+document.querySelector(".back").onclick = () => {
+  if (pageNum > 1) {
+    pageNum -= 1;
+  }
+  if (pageNum <= 1) {
+    document.querySelector(".back").classList.remove("active");
+  }
+
+  if (pageNum < 10) {
+    imageNum = `00${pageNum}`;
+  } else if (pageNum < 100) {
+    imageNum = `0${pageNum}`;
+  } else {
+    imageNum = pageNum;
+  }
+
+  let image = document.querySelector(".img img");
+
+  image.setAttribute(
+    "src",
+    `https://www.searchtruth.org/quran/images1/${imageNum}.jpg`
+  );
+};
+
+async function getSurah(name) {
+  const res = await fetch("https://api.alquran.cloud/v1/surah");
+  const data = await res.json();
+
+  let surahNum = 1;
+  data.data.forEach((el) => {
+    if (name == el.name) {
+      surahNum = el.number;
+    } else if (name == el.englishName) {
+      surahNum = el.number;
+    }
+  });
+
+  if (surahNum < 10) {
+    imageNum = `00${surahNum}`;
+  } else if (surahNum < 100) {
+    imageNum = `0${surahNum}`;
+  } else {
+    imageNum = surahNum;
+  }
+
+  let image = document.querySelector(".img img");
+
+  image.setAttribute(
+    "src",
+    `https://www.searchtruth.org/quran/images1/${imageNum}.jpg`
+  );
+}
+
+let list = document.querySelector(".select ul");
+let select = document.querySelector(".selectField");
+let selectText = document.querySelector(".selectField p");
+
+async function listData() {
+  const res = await fetch("https://api.alquran.cloud/v1/surah");
+  const data = await res.json();
+  data.data.forEach((el) => {
+    let li = document.createElement("li");
+    li.innerHTML = el.name;
+    list.appendChild(li);
+  });
+
+  let selecLi = document.querySelectorAll(".select ul li");
+  selecLi.forEach((li) => {
+    li.onclick = () => {
+      selectText.innerHTML = li.textContent;
+      data.data.forEach((x) => {
+        if (x.name == li.textContent) {
+          let surahNum = x.number;
+          pageNum = x.number;
+          if (pageNum > 1) {
+            document.querySelector(".back").classList.add("active");
+          }
+          if (pageNum <= 1) {
+            document.querySelector(".back").classList.remove("active");
+          }
+          if (surahNum < 10) {
+            imageNum = `00${surahNum}`;
+          } else if (surahNum < 100) {
+            imageNum = `0${surahNum}`;
+          } else {
+            imageNum = surahNum;
+          }
+          let image = document.querySelector(".img img");
+
+          image.setAttribute(
+            "src",
+            `https://www.searchtruth.org/quran/images1/${imageNum}.jpg`
+          );
+        }
+      });
+    };
+  });
+}
+
+select.onclick = () => {
+  list.classList.toggle("hide");
+};
